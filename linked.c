d62a4e1644cafa90f8a783b2d46bf8d310069b50
KosMosV2 <jesusm1948@gmail.com>
2024-07-20 17:56:19 -0400

    added insertHead

--- linked.c
+++ linked.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Node Node;
+
+struct Node{
+    int data;
+    Node * next;
+};
+
+void insertHead(Node ** head, int data){
+    Node * newNode = (Node *) malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = *head;
+
+    *head = newNode;
+}
+
 int main(){
+    Node * head = NULL;
     
+    insertHead(&head, 5);
+    insertHead(&head, 4);
+    insertHead(&head, 3);
+    insertHead(&head, 2);
+    insertHead(&head, 1);
+    insertHead(&head, 0);
+
+    while(head != NULL){
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL");
+    printf("\n");
 }
